454bacd5cc6505393283a129356d8a06966b29d7
kashifmurtaza72 <kashifmurtaza72@gmail.com>
2024-03-14 07:34:38 +0500

    token

--- jwt.js
+++ jwt.js
@@ -1,10 +1,9 @@
 const jwt = require("jsonwebtoken");
 
 const jwtAuthMiddleware = (req, res, next) => {
-
   // First check header has authorization or not
   const authorization = req.headers.authorization;
-  if (!authorization) return res.status(401).json({error:'Token not found'})
+  if (!authorization) return res.status(401).json({ error: "Token not found" });
 
   // Extract the jwt token from the request headers
   const token = req.headers.authorization.split(" ")[1];
@@ -22,13 +21,11 @@ const jwtAuthMiddleware = (req, res, next) => {
   }
 };
 
-
-
 // Function to generate JWT Token
 const generateToken = (userData) => {
-    // generate a new JWT token using user data
-    return jwt.sign(userData, process.env.JWT_SECRET, {expiresIn:3000000});
-}
+  // generate a new JWT token using user data
+  return jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: 3000000 });
+};
 
 //module.exports = jwtAuthMiddleware;
-module.exports = {jwtAuthMiddleware, generateToken};
+module.exports = { jwtAuthMiddleware, generateToken };
